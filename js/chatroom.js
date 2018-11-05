eb036407840a31f905ef5577a768a89be1baf058
TomekMichalski <tmm.michalski@gmail.com>
2018-11-05 21:43:20 +0100

    v0.1.3dev

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,10 +1,48 @@
-$('.chatroom__form__inputfile').bind('change', function () {
-    var filename = $(".chatroom__form__inputfile").val();
-    if (/^\s*$/.test(filename)) {
-      $(".chatroom__form__attach").removeClass('chatroom__form__attach--active');
-    }
-    else {
-      $(".chatroom__form__attach").addClass('chatroom__form__attach--active');
-    }
-  });
-  
+const clockHtml = document.querySelector(".chatroom__time__clock");
+const dateHtml = document.querySelector(".chatroom__time__date");
+
+const months = ["stycznia","lutego","marca","kwietnia","maja","czerwca","lipca","sierpnia","września","października","listopada","grudnia"];
+
+var showHour = () => {
+  //pobierz wartości do zmiennej
+  let date = new Date();
+  let hour = date.getHours(); 
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+
+  // jeżeli godzina równa 0 zastąp ją 12
+  if(hour == 0) hour = 12;
+
+  //wstaw 0 przed wartość jeżeli poniżej 10
+  hour = (hour < 10) ? "0" + hour : hour;
+  minute = (minute < 10) ? "0" + minute : minute;
+  second = (second < 10) ? "0" + second : second;
+
+  //utwórz gotowy łańcuch
+  let currentTime = `${hour}:${minute}:${second}`;
+  //zaktualizuj date
+  clockHtml.innerText = currentTime;
+
+  //po sekundzie wywołaj funkcje - rekurencja
+  setTimeout(showHour, 1000);
+}
+
+
+var showDate = () => {
+  //pobierz wartości daty
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  //utwórz gotowy string
+  let currentDate = `${day} ${months[month]}, ${year}`;
+
+  //zaktualizuj date
+  dateHtml.innerText = currentDate;
+
+  //rekurencja jest niewymagana - aktualizacja tylko po załadowaniu
+}
+
+// wywołaj funkcje po załadowaniu
+showHour();
+showDate();
